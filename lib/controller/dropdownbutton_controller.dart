ffc0df92f62f88ce7887c5ccfa83c460ba283bcc
iamubaid370 <meubaid.pks@gmail.com>
2024-08-07 17:34:11 +0500

    sql Lite Issue and Uploaded Code till 07/Aug/2024 new comit

---
+++ lib/controller/dropdownbutton_controller.dart
@@ -0,0 +1,39 @@
+import 'package:all_languages_voice_dictionary/View/home_screen/homescreen_controller.dart';
+import 'package:get/get.dart';
+
+import '../utils/get_language_code.dart';
+
+class DropDownButtonController extends GetxController{
+  var translatedText = ''.obs;
+
+  HomeScreenController homeScreenController = Get.put(HomeScreenController());
+   getLangCode(String language){
+    String? languageCode = getLanguageCode(language);
+    if(languageCode != null){
+      print(languageCode.toString());
+
+      try{
+        homeScreenController.searchContain(homeScreenController.textEditingController.text, languageCode);
+        //homeScreenController.dropDownValue2.value = languageCode;
+      }
+      catch(e){print(e.toString());}
+
+
+    }
+   }
+   languageCode(String language) async {
+     String? languageCode = getLanguageCode(language);
+     if (languageCode != null) {
+       String translation = await oneWordMeaning(homeScreenController.textEditingController.text, languageCode);
+       translatedText.value = translation;
+     }
+   }
+
+   oneWordMeaning(String text,String targetLanguage){
+     final translatedText = homeScreenController.translateText(text, targetLanguage);
+     return translatedText;
+   }
+
+
+}
+
